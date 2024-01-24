08b523722c627c474dea5f14bdeeb21689c8328b
artem <art.zavtur@gmail.com>
2024-01-24 02:14:41 +0200

    foot and ball

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
@@ -11,21 +12,40 @@ class GameScreen extends StatefulWidget {
 
 class _GameScreenState extends State<GameScreen> {
   final _game = MyGame();
+
   @override
   Widget build(BuildContext context) {
-    return GameWidget(game: _game);
+    return Stack(
+      children: [
+        GameWidget(game: _game),
+        SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).pop(),
+            child: Image.asset(
+              'assets/images/menu.png',
+            ),
+          ),
+        )),
+      ],
+    );
   }
 }
 
 class MyGame extends FlameGame {
   MyGame();
 
+  late BallSprite ball;
+  late BootSprite boot;
+
   @override
   Future<void> onLoad() async {
     addAll([
       BackGround(),
+      ball = BallSprite(),
+      boot = BootSprite(),
     ]);
-    // interval.onTick = () => add(BubblesGroup());
   }
 }
 
@@ -35,8 +55,45 @@ class BackGround extends SpriteComponent {
   @override
   Future<void> onLoad() async {
     sprite = await Sprite.load(
-      'bg_5.png',
+      'bg_1.png',
       srcPosition: Vector2(50, 50),
     );
+  }
+}
+
+class BallSprite extends SpriteComponent with HasGameRef<MyGame>, CollisionCallbacks {
+  BallSprite() : super(priority: 1);
+
+  @override
+  Future<void> onLoad() async {
+    final ball = await gameRef.loadSprite('ball.png');
+    size = Vector2(100, 100);
+    position = Vector2(
+      gameRef.size.x / 2 - 50,
+      gameRef.size.y / 1.7 - 50,
+    );
+    sprite = ball;
+  }
+
+  @override
+  void update(double dt) {
+    position -= Vector2(0, -1) * dt * 300;
+    super.update(dt);
+  }
+}
+
+class BootSprite extends SpriteComponent with HasGameRef<MyGame>, CollisionCallbacks {
+  BootSprite() : super(priority: 1);
+
+  @override
+  Future<void> onLoad() async {
+    final boot = await gameRef.loadSprite('boot_1.png');
+    size = Vector2(157, 100);
+    position = Vector2(
+      gameRef.size.x / 2 - 50,
+      gameRef.size.y / 1.7 - 50,
+    );
+    sprite = boot;
+    angle = -0.5;
   }
 }
