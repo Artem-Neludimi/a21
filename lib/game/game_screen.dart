fedacca681b172c021caacfb890b6bcbc4383afe
artem <art.zavtur@gmail.com>
2024-01-24 17:45:52 +0200

    idk

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -38,10 +38,21 @@ class _GameScreenState extends State<GameScreen> {
 class MyGame extends FlameGame with PanDetector {
   MyGame();
 
-  late BallSprite _ball;
   late BootSprite _boot;
+  late BallSprite _ball;
 
+  //boot state
   bool _isTap = false;
+  Vector2 _bootPosition = Vector2.zero();
+
+  //ball state
+  bool _toTop = false;
+  final bool _toLeft = false;
+  final bool _toRight = false;
+  final double _bottomSpeed = 300;
+  double _topSpeed = 300;
+  final double _leftSpeed = 300;
+  final double _rightSpeed = 300;
 
   @override
   Future<void> onLoad() async {
@@ -55,8 +66,8 @@ class MyGame extends FlameGame with PanDetector {
 
   @override
   void update(double dt) {
-    _manageBallPosition(dt);
     _manageFootAngle();
+    _manageBallPosition(dt);
     super.update(dt);
   }
 
@@ -69,6 +80,7 @@ class MyGame extends FlameGame with PanDetector {
 
   @override
   void onPanUpdate(DragUpdateInfo info) {
+    _bootPosition = info.eventPosition.global;
     _provideBootPosition(info.eventPosition.global);
     super.onPanUpdate(info);
   }
@@ -76,11 +88,48 @@ class MyGame extends FlameGame with PanDetector {
   @override
   void onPanEnd(DragEndInfo info) {
     _isTap = false;
+    _bootPosition = Vector2.zero();
     super.onPanEnd(info);
   }
 
   void _manageBallPosition(double dt) {
-    _ball.position -= Vector2(0, -1) * dt * 300;
+    if (_bootPosition.y > _ball.position.y &&
+        _bootPosition.y < _ball.position.y + 100 &&
+        _ball.position.x > _bootPosition.x - 150 &&
+        _ball.position.x < _bootPosition.x + 50) {
+      _ball.position.y = _bootPosition.y - 100;
+      _setTopTrue();
+    }
+    _ballToBottom(dt);
+    _ballToTop(dt);
+    _ballToRight(dt);
+    _ballToLeft(dt);
+  }
+
+  _ballToBottom(double dt) {
+    final y = _ball.position.y;
+    if (y <= size.y - _ball.size.y) {
+      _ball.position -= Vector2(0, -1) * dt * _bottomSpeed;
+    }
+  }
+
+  void _ballToTop(double dt) {
+    if (_toTop == false) return;
+    _ball.position -= Vector2(0, 1) * dt * _topSpeed;
+  }
+
+  void _ballToLeft(double dt) {
+    if (_toLeft == false) return;
+    if (_ball.position.x >= 0) {
+      _ball.position -= Vector2(1, 0) * dt * _leftSpeed;
+    }
+  }
+
+  void _ballToRight(double dt) {
+    if (_toRight == false) return;
+    if (_ball.position.x <= size.x - _ball.size.x) {
+      _ball.position -= Vector2(-1, 0) * dt * _rightSpeed;
+    }
   }
 
   void _provideBootPosition(Vector2 position) {
@@ -88,6 +137,13 @@ class MyGame extends FlameGame with PanDetector {
       position.x - _boot.size.x / 2,
       position.y - _boot.size.y / 2,
     );
+  }
+
+  void _setTopTrue() async {
+    _toTop = true;
+    _topSpeed = 600;
+    await Future.delayed(const Duration(milliseconds: 500));
+    _toTop = false;
   }
 
   void _manageFootAngle() {
@@ -138,5 +194,9 @@ class BootSprite extends SpriteComponent with HasGameRef<MyGame>, CollisionCallb
       gameRef.size.y / 1.7 - 50,
     );
     sprite = boot;
+    position = Vector2(
+      gameRef.size.x / 2.5 - 50,
+      gameRef.size.y / 1.2 - 50,
+    );
   }
 }
